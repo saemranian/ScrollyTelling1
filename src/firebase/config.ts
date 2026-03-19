6f6dd3e1e11b527812b7c240d14c530e42028093
Ahmad Emranian <saemranian@gmail.com>
2026-03-19 08:56:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-195804607-98aec",
+  "appId": "1:1089119451187:web:c6b9cc649a1d4cbffbcb46",
+  "apiKey": "AIzaSyBPX48uYuLG_wPucEsdPcDxZtkD7C-U1Us",
+  "authDomain": "studio-195804607-98aec.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1089119451187"
+};
